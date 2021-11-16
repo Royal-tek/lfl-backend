3068f96ee95768f27df6dd40c2f635cedfc1c63d
Your Name <you@example.com>
2021-11-16 21:20:29 +0100

    New Update

--- landmark_fantasy_app/settings.py
+++ landmark_fantasy_app/settings.py
@@ -168,7 +168,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATICFILES_DIRS = [BASE_DIR / 'static']
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
